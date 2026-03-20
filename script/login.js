2ac03b34bd7924a2d7d28d85c60f3e8f8beb536b
faisalalfin <faisalalfin13@gmail.com>
2026-03-20 23:28:32 +0600

    sign in functionalities and homepage added successfully

--- script/login.js
+++ script/login.js
@@ -1,1 +1,29 @@
 console.log("Login Successful")
+
+document.getElementById("login-btn").addEventListener("click", function(){
+    //1- get the username
+
+    const nameInput = document.getElementById("input-name");
+    const username = nameInput.value;
+    console.log(username);
+
+    //2- get the password
+    const passInput = document.getElementById("input-pass");
+    const password = passInput.value;
+    console.log(password);
+
+    //3- match username and password
+
+    if (username == "admin" && password == "admin123"){
+        alert("login success");
+
+        window.location.assign("/home.html");
+    }
+
+    else {
+        alert("login failed");
+        return;
+    }
+
+
+})
